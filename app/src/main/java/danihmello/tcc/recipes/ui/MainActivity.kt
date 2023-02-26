b7629b80595ad19dc216755aa2727886c4fa6729
Daniela Heckler <danihmello06@gmail.com>
2023-02-26 10:24:11 -0300

    Added navigation in bottom navigation bar

--- app/src/main/java/danihmello/tcc/recipes/ui/MainActivity.kt
+++ app/src/main/java/danihmello/tcc/recipes/ui/MainActivity.kt
@@ -3,18 +3,36 @@ package danihmello.tcc.recipes.ui
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import danihmello.tcc.recipes.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import danihmello.tcc.recipes.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val navController: NavController by lazy { findNavController(R.id.navHostFragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        with(binding.bottomNavigationView) {
+            setupWithNavController(navController)
+
+            setOnItemSelectedListener {
+                navController.navigate(it.itemId)
+                true
+            }
+
+        }
     }
 
 }
