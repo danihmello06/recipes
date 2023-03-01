c6631248f607afe76e4bd46b97c508de1eb80616
Daniela Heckler <danihmello06@gmail.com>
2023-03-01 18:27:12 -0300

    added bottom navigation bar and implemented navigation

--- app/src/main/java/danihmello/tcc/recipes/ui/FavoritesFragment.kt
+++ app/src/main/java/danihmello/tcc/recipes/ui/FavoritesFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import danihmello.tcc.recipes.R
 
-class SavedRecipes : Fragment() {
+class FavoritesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_saved_recipes, container, false)
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
 
 
--- app/src/main/java/danihmello/tcc/recipes/ui/HomeFragment.kt
+++ app/src/main/java/danihmello/tcc/recipes/ui/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
 import androidx.core.os.bundleOf
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import danihmello.tcc.recipes.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
